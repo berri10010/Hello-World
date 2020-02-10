432cee47a5fb2a90c3850231b4d54a8ff948fa42
berri10010 <53055829+berri10010@users.noreply.github.com>
2020-02-10 09:32:09 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+  // I think this is a comment
+  // main is the point of entry for the program
+  // shortcut: type 'main' and press ctrl + space bar
+
+  public static void main (String[] args) {
+
+    // prints "stoopit!" to the console!"
+    // printin shortcut: type 'syso' and press ctrl + space bar
+
+    System.out.println("stoopit!");
+    System.out.println("stoopit!");
+    System.out.println("stoopit!");
+    
+  }
+}
+// Main.java is HelloWorld.java in the Udemy course
