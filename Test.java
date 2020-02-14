0d49c4ce912e44d14b4af036316be5eebd23b198
Berri <53055829+berri10010@users.noreply.github.com>
2020-02-14 09:37:56 -0500

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,13 @@
+import javax.swing.JLabel;
+
+//import tutorials.basic.Person; is not needed anymore
+
+public class Test {
+	public static void main(String[] args) {
+		tutorials.basic.Person person = new tutorials.basic.Person("");
+		Person person2 = new Person("");
+		
+		JLabel jbl;
+	}
+
+}
